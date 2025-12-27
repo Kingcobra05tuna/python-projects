7fcd10b9d6aa5f5923f15b00c93dedcbfcaca67b
Kingcobra05tuna <tunagulagac@gmail.com>
2025-12-27 19:01:08 +0300

    update

--- NumberGuessing/main.py
+++ NumberGuessing/main.py
@@ -1,8 +1,10 @@
 from random import randint
 
-DIFFICULTIES = {"easy":10, "medium":100, "hard":1_000, "extreme":10_000, "impossible":1_000_000}
+DIFFICULTIES =  {
+    "easy": 10, "medium": 100, "hard": 1_000,
+    "extreme": 10_000, "impossible": 1_000_000}
 
-def main():
+def game():
     # Get valid input
     while True:
         d = input(f"Difficulty {tuple(DIFFICULTIES)}: ").strip().lower()
@@ -16,23 +18,26 @@ def main():
     # Guessing until finding the goal
     while True:
         try:
-            n = int(input(f"Guess (1 - {DIFFICULTIES[d]}): "))
-            if not 1 <= n <= DIFFICULTIES[d]: raise ValueError
+            num = int(input(f"Guess (1 - {DIFFICULTIES[d]}): "))
+            if not 1 <= num <= DIFFICULTIES[d]: raise ValueError
         except ValueError:
             print("Invalid input.")
             continue
 
         attempts += 1
-        if n < goal:
+        if num < goal:
             print("Higher!")
-        elif n > goal:
+        elif num > goal:
             print("Lower!")
         else:
             print(f"You won in {attempts} attempts!")
             break
 
-    # Replay
-    if input("Try again? (y/n): ").lower()[0] == "y": main()
+# Makes the game replayable
+def main():
+    while True:
+        game()
+        if input("Try again? (y/n): ").lower()[0] != "y": break
 
 if __name__ == "__main__":
     main()
